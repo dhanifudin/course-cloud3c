ddffd279de92196438fc7f2539e178cd15e0870b
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-09-11 19:20:21 +0700

    Initial commit

---
+++ .github/test/test.js
@@ -0,0 +1,22 @@
+const glob = require("glob");
+const { countCharacters } = require("wcmd/lib/utils");
+const MINIMAL_CHARS = 300;
+
+(async () => {
+  glob("../../attendances/**/*.md", (err, files) => {
+    files.forEach((file) => {
+      const result = countCharacters(file);
+      const chars = result.lines
+        .map((line) => line.length)
+        .reduce((l1, l2) => l1 + l2);
+      if (chars < MINIMAL_CHARS) {
+        console.error(
+          `${file} doesn't meet summary requirements (minimum ${MINIMAL_CHARS} chars): ${chars} chars`
+        );
+        process.exit(1);
+      }
+    });
+    console.log(`All summary passed`);
+    process.exit(0);
+  });
+})();
